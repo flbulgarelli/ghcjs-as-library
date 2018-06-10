7eac36a45c3ddf26ba82f67368fa5ab25860538b
Franco Bulgarelli <franco@mumuki.org>
2018-06-10 12:55:52 -0300

    Wrapper functions signatures more consistent

--- ghcjslib/src/binding.js
+++ ghcjslib/src/binding.js
@@ -6,25 +6,36 @@ function ghcjsApply(fun, arg) {
   return h$c2(h$ap1_e, fun, arg);
 }
 
-function ghcjsWrapperString(functionName) {
-  let fun = ghcjsBinding(functionName);
-  return () => h$runSyncReturn(fun) ;
+function ghcjsString(string) {
+  return h$c1(h$ghcjszmprimZCGHCJSziPrimziJSVal_con_e, string);
 }
 
-function ghcjsWrapperUnit(functionName) {
-  let fun = ghcjsBinding(functionName);
-  return () => { h$runSync(fun)} ;
+function ghcjsWrapper(code) {
+  return (functionName) => code(ghcjsBinding(functionName));
 }
 
-function ghcjsWrapperPrimitiveUnit(functionName) {
-  let fun = ghcjsBinding(functionName);
-  return (arg) => { h$runSync(ghcjsApply(fun, arg)) };
-}
+// Wrappers
 
-let ghcjsWrapperNumberUnit = ghcjsWrapperPrimitiveUnit;
-let ghcjsWrapperBooleanUnit = ghcjsWrapperPrimitiveUnit;
+// * Any
+//    f :: IO (JSVal | JSString)
+const ghcjsWrapperUnit = ghcjsWrapper((fun) => () => { h$runSync(fun)});
 
-function ghcjsWrapperStringUnit(functionName) {
-  let fun = ghcjsBinding(functionName);
-  return (arg) => { h$runSync(ghcjsApply(fun, h$c1(h$ghcjszmprimZCGHCJSziPrimziJSVal_con_e, arg))) };
-}
+// * Unit
+//    f :: IO ()
+const ghcjsWrapperAny = ghcjsWrapper((fun) => () => h$runSyncReturn(fun));
+
+// * ValueUnit
+//    f :: (Int | Bool | JSVal) -> IO ()
+const ghcjsWrapperValueUnit = ghcjsWrapper((fun) => (arg) => { h$runSync(ghcjsApply(fun, arg)) });
+
+// * ValueAny
+//    f :: (Int | Bool | JSVal) -> IO (JSVal | JSString)
+const ghcjsWrapperValueAny = ghcjsWrapper((fun) => (arg) => h$runSyncReturn(ghcjsApply(fun, arg)));
+
+// * StringUnit
+//    f :: JSString -> IO ()
+const ghcjsWrapperStringUnit = ghcjsWrapper((fun) => (arg) => { h$runSync(ghcjsApply(fun, ghcjsString(arg))) });
+
+// * StringAny
+//    f :: JSString -> IO (JSVal | JSString)
+const ghcjsWrapperStringAny = ghcjsWrapper((fun) => (arg) => h$runSyncReturn(ghcjsApply(fun, ghcjsString(arg))));
--- ghcjslib/src/exports.js
+++ ghcjslib/src/exports.js
@@ -1,11 +1,17 @@
 let cambore = {
   main: ghcjsWrapperUnit("main"),
+  // ukaraaIO :: IO ()
   ukaraaIO: ghcjsWrapperUnit("ukaraaIO"),
-  kemeroonIO: ghcjsWrapperNumberUnit("kemeroonIO"),
+  // kemeroonIO :: Int -> IO ()
+  kemeroonIO: ghcjsWrapperValueUnit("kemeroonIO"),
+  // xinaiuIO :: String -> IO ()
   xinaiuIO: ghcjsWrapperStringUnit("xinaiuIO"),
-  laimaDaraaIO: ghcjsWrapperString("laimaDaraaIO"),
-  volvioBelixaMelicaanIO: ghcjsWrapperString("volvioBelixaMelicaanIO"),
-  cantidadDeSupremosIO: ghcjsWrapperString("cantidadDeSupremosIO")
+  // laimaDaraaIO :: IO JSString
+  laimaDaraaIO: ghcjsWrapperAny("laimaDaraaIO"),
+  // volvioBelixaMelicaanIO :: IO JSVal
+  volvioBelixaMelicaanIO: ghcjsWrapperAny("volvioBelixaMelicaanIO"),
+  // cantidadDeSupremosIO :: IO JSVal
+  cantidadDeSupremosIO: ghcjsWrapperAny("cantidadDeSupremosIO")
 };
 
 if (typeof module !== 'undefined') {
